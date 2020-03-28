a5a2dc77f950d10e017c3da839eb8f310c151844
Jan Suchotzki <jan@lernmoment.de>
2020-03-28 11:11:19 +0100

    Projektdateien hinzufügen.

---
+++ CastTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CastTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short aShortValue = 10;
+            int anIntValue = short.MaxValue + 1;
+
+            //aShortValue = anIntValue; //CS0266 - Der Typ "int" kann nicht implizit in "short" konvertiert werden.
+
+            aShortValue = (short)anIntValue;
+            Console.WriteLine($"anIntValue hat den Wert {anIntValue}");
+            Console.WriteLine($"aShortValue hat den Wert {aShortValue} nach dem explicit Cast");
+        }
+    }
+}
